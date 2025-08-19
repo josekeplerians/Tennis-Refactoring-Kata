0c13b1d91e8accdeeef4241ef33978e5d8668c92
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-08-19 11:55:12 +0200

    refactor: using TranslateToScoring

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -35,33 +35,37 @@ namespace Tennis
             }
             else if (AnyPlayerInAdvantageOrWin)
             {
-                var minusResult = _mScore1 - _mScore2;
-                score = minusResult switch
-                {
-                    1 => "Advantage player1",
-                    -1 => "Advantage player2",
-                    >= 2 => "Win for player1",
-                    _ => "Win for player2"
-                };
+                score = Choosejkhgasjffddga();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    var tempScore = 0;
-                    if (i == 1) tempScore = _mScore1;
-                    else { score += "-"; tempScore = _mScore2; }
-
-                    score += tempScore switch
-                    {
-                        0 => "Love",
-                        1 => "Fifteen",
-                        2 => "Thirty",
-                        3 => "Forty",
-                        _ => ""
-                    };
-                }
+                score = $"{TranslateToScoring(_mScore1)}-{TranslateToScoring(_mScore2)}";
             }
+            return score;
+        }
+
+        private static string TranslateToScoring(int tempScore)
+        {
+            return tempScore switch
+            {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => ""
+            };
+        }
+
+        private string Choosejkhgasjffddga()
+        {
+            var minusResult = _mScore1 - _mScore2;
+            var score = minusResult switch
+            {
+                1 => "Advantage player1",
+                -1 => "Advantage player2",
+                >= 2 => "Win for player1",
+                _ => "Win for player2"
+            };
             return score;
         }
 
