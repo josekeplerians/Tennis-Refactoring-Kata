697d328bd6abfd165f9eeb0ed2b40b93fc8ffeee
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-09-16 11:43:20 +0200

    refactor: swap _p1Points to _p2Points (incorrect naming)

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -17,40 +17,40 @@ namespace Tennis
 
         public string GetScore()
         {
-            if (_p2Points < 4 && _p1Points < 4 && _p2Points + _p1Points < 6)
+            if (_p1Points < 4 && _p2Points < 4 && _p1Points + _p2Points < 6)
             {
                 string[] scoreNames = ["Love", "Fifteen", "Thirty", "Forty"];
-                var player2ScoreName = scoreNames[_p2Points];
-                var player1ScoreName = scoreNames[_p1Points];
+                var player2ScoreName = scoreNames[_p1Points];
+                var player1ScoreName = scoreNames[_p2Points];
                 return IsTie ? player2ScoreName + "-All" : player2ScoreName + "-" + player1ScoreName;
             }
 
             if (IsTie)
                 return "Deuce";
             var playerWithMorePoints = WhoHasMorePoints();
-            return gdfghdfggh(playerWithMorePoints);
+            return GetAdvantageOrWinScore(playerWithMorePoints);
         }
 
         private string WhoHasMorePoints()
         {
-            return _p2Points > _p1Points ? _p1Name : _p2Name;
+            return _p1Points > _p2Points ? _p1Name : _p2Name;
         }
 
-        private string gdfghdfggh(string player)
+        private string GetAdvantageOrWinScore(string player)
         {
             return (PointsDifference()  == 1) ? "Advantage " + player : "Win for " + player;
         }
 
-        private int PointsDifference() => Math.Abs(_p2Points - _p1Points);
+        private int PointsDifference() => Math.Abs(_p1Points - _p2Points);
 
-        private bool IsTie => _p2Points == _p1Points;
+        private bool IsTie => _p1Points == _p2Points;
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                _p2Points += 1;
-            else
                 _p1Points += 1;
+            else
+                _p2Points += 1;
         }
 
     }
