3018dd49c56727d8c93eb14c39d4cef0319deb88
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-09-16 11:13:25 +0200

    refactor: rename variables to be more descriptive

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int _p2;
-        private int _p1;
+        private int _p1Points;
+        private int _p2Points;
         private readonly string _p1Name;
         private readonly string _p2Name;
 
@@ -16,25 +16,25 @@ namespace Tennis
         public string GetScore()
         {
             string s;
-            if (_p1 < 4 && _p2 < 4 && _p1 + _p2 < 6)
+            if (_p2Points < 4 && _p1Points < 4 && _p2Points + _p1Points < 6)
             {
                 string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[_p1];
-                return (_p1 == _p2) ? s + "-All" : s + "-" + p[_p2];
+                s = p[_p2Points];
+                return (_p2Points == _p1Points) ? s + "-All" : s + "-" + p[_p1Points];
             }
 
-            if (_p1 == _p2)
+            if (_p2Points == _p1Points)
                 return "Deuce";
-            s = _p1 > _p2 ? _p1Name : _p2Name;
-            return ((_p1 - _p2) * (_p1 - _p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = _p2Points > _p1Points ? _p1Name : _p2Name;
+            return ((_p2Points - _p1Points) * (_p2Points - _p1Points) == 1) ? "Advantage " + s : "Win for " + s;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                _p1 += 1;
+                _p2Points += 1;
             else
-                _p2 += 1;
+                _p1Points += 1;
         }
 
     }
